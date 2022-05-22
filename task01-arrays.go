3b6d4f23e9c4d925a14c5c40bb72ac027a1b5712
Pavel Dubrov <n1sh4dow@gmail.com>
2022-05-22 13:49:45 +0300

    solution v1.0

--- task01-arrays.go
+++ task01-arrays.go
@@ -1,6 +1,14 @@
 package homework
 
 func average(input [15]float32) (result float32) {
-	//Place your code here
-	return
+	var sum float32
+	var elements float32
+	sum = 0
+	elements = float32(len(input))
+
+	for i := 0; i < len(input); i++ {
+		sum += input[i]
+	}
+	result = sum / elements
+	return result
 }
--- task03-map.go
+++ task03-map.go
@@ -1,6 +1,16 @@
 package homework
 
+import "sort"
+
 func sortMapValues(input map[int]string) (result []string) {
 	//Place your code here
-	return
+	var my_slice []int
+	for k, _ := range input {
+		my_slice = append(my_slice, k)
+	}
+	sort.Ints(my_slice)
+	for _, elem := range my_slice {
+		result = append(result, input[elem])
+	}
+	return result
 }
